c636922b350625481292ed22f0c100f77b804389
Rakesh Patel <rakesh246808@gmail.com>
2024-02-11 11:21:43 +0530

    added some gsap animations

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 let scrollDownArrow = document.querySelector(".scrollDownArrow");
 
-scrollDownArrow.classList.toggle("scrollBtnShowHide", window.scrollY > 100);
+window.onscroll = (e) => {
+    scrollDownArrow.classList.toggle("scrollBtnShowHide", window.scrollY > 100);
+}
+
 
 let earth = document.querySelector('.earth');
 
 let tl = gsap.timeline({
-    scrollTrigger: {
-        trigger: '.earth',
-        start:'top 60%',
-        end:'bottom 80%',
-        scrub: true,   
-        markers:true
-    }
+  
 })
 
 tl.to('.earth', {
     x:-1150,
     y:450,
-    rotate:360
-})
+    rotate:360,
+    scrollTrigger: {
+        trigger: '.earth',
+        start:'top 60%',
+        end:'bottom 60%',
+        scrub: true,   
+        // markers:true
+    }
+});
+
+
+//[[[[[[[[[[[[[[[[[[[[[[[[[[*************************** second page  ***********************]]]]]]]]]]]]]]]]]]]]]
+let secondPagePlanetDetails = document.querySelector('.planet-details');
+tl.from('.planet-details', {
+    x:500,
+    y:0,
+    opacity:0,
+    // rotate:360,
+    scrollTrigger: {
+        trigger: '.planet-details',
+        start:'top 100%',
+        end:'bottom 60%',
+        scrub: true,   
+        // markers:true
+    }
+});
